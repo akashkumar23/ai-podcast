cc2773c60529a4145dcef1666bacb56f93b4cd23
Akash Kumar <77935092+akashkumar23@users.noreply.github.com>
2024-06-08 08:15:23 +0530

    implement clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://mutual-seasnail-91.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
--- convex/https.ts
+++
@@ -1,1 +0,0 @@
-// create a clerk user -> info -> Users.create
